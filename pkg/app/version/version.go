1562aaa7f9f66c0ce2abe918d13e4a3fab364d4b
Quan TRAN <account@itscaro.me>
2022-06-15 12:49:45 +0200

    on canary build, version should show "0.0.0-dev"
    Signed-off-by: Quan TRAN <account@itscaro.me>

--- pkg/app/version/version.go
+++ pkg/app/version/version.go
@@ -1,3 +1,24 @@
 package version
 
+import "fmt"
+
 var Version string
+var Commit string
+
+func GetVersion() string {
+	if Version == "" {
+		Version = "0.0.0-dev"
+	}
+	return Version
+}
+
+func GetCommit() string {
+	if Commit == "" {
+		Commit = "unknown_commit"
+	}
+	return Commit
+}
+
+func GetVersionWithCommit() string {
+	return fmt.Sprintf("%s-%s", GetVersion(), GetCommit())
+}
